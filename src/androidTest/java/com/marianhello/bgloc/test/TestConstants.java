2a1f245e03bcec26433bee3a4cc67eab116721f3
Quang <quang@codelynx.io>
2021-01-07 01:33:37 +0700

    fix missing resource

--- src/androidTest/java/com/marianhello/bgloc/test/TestConstants.java
+++ src/androidTest/java/com/marianhello/bgloc/test/TestConstants.java
@@ -1,5 +1,5 @@
 package com.marianhello.bgloc.test;
 
 abstract public class TestConstants {
-    public static final String Authority = "com.marianhello.app.mauron85.bgloc.provider";
+    public static final String Authority = "com.marianhello.app.ntquang98.bgloc.provider";
 }
